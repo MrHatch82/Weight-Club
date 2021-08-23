4b6d9d360894ea3532def491395f3e4a02a81a71
MrHatch <christof@hierwirdgeloungt.de>
2021-08-23 23:18:57 +0200

    random subtitles

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,36 @@
 <template>
   <div class="container text-center pt-5">
     <h1>WEIGHT CLUB</h1>
+    <div v-if="rando !== false" class="subtitle" v-html="subtitles[rando]" />
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      rando: false,
+      subtitles: [
+        'Get off your bum!',
+        'Competitive weight loss made easy!',
+        'Have you tried not eating so much?',
+        "You got fat by sitting in a chair all day? Who'd've thought!",
+        'Now with extra stats!',
+        "If you can read this, you're too heavy!",
+        "Wouldn't it be nice to see those toes again?",
+        'Lose to win!',
+        'Move it, you fat f*ck!'
+      ]
+    }
+  },
+  mounted() {
+    this.rando = Math.floor(Math.random() * this.subtitles.length)
+  }
+}
 </script>
 
-<style lang="sass">
+<style lang='scss'>
+.subtitle {
+  color: #aaa;
+}
 </style>
