08ef96f0ed3879b45e15d9a7a3dd896fbee4bbb7
MrHatch <christof@hierwirdgeloungt.de>
2021-08-27 16:18:12 +0200

    proper linter settings finally!

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,18 +2,20 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
     parser: '@babel/eslint-parser',
-    requireConfigFile: false
+    requireConfigFile: false,
   },
   extends: ['@nuxtjs', 'plugin:nuxt/recommended'],
   plugins: [],
   // add your custom rules here
   rules: {
-    'space-before-function-paren': 'off',
-    'vue/no-v-html': 'off',
-    'vue/html-self-closing': 'off'
-  }
-}
+    'space-before-function-paren': 0,
+    'vue/no-v-html': 0,
+    'vue/html-self-closing': 0,
+    'comma-dangle': [2, 'always-multiline'],
+    semi: [2, 'always'],
+  },
+};
