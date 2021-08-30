005792ed15309a4447179ab58ca9b19f88b42cc5
MrHatch <christof@hierwirdgeloungt.de>
2021-08-30 18:09:37 +0200

    topnav // added tab select animation

--- components/topnav.vue
+++ components/topnav.vue
@@ -32,6 +32,7 @@
   transform: translateY(-100%);
   transition: transform 0.5s 0.5s ease-in-out;
   z-index: 20;
+  overflow: hidden;
 
   &.show {
   transform: translateY(0);
@@ -46,7 +47,7 @@
 
   .tab {
     display: inline-block;
-    padding: 1rem;
+    padding: 1rem 1rem 0.5rem;
     min-width: 200px;
     margin: 0 0.25rem -1rem;
     border-radius: 6px 6px 0 0;
@@ -56,19 +57,23 @@
     background: #2d2d2d;
     cursor: pointer;
     box-shadow: inset 0px -5px 5px -4px rgba(0,0,0,0.15);
-    transition: color 0.25s, background 0.25s 0.25s, box-shadow 0.25s 0.25s;
+    transition: padding 0.25s ease-in-out, color 0.25s, background 0.25s 0.25s, box-shadow 0.25s 0.25s , transform 0.25s ease-in-out;
     color: darken($light, 15%);
     text-decoration: none;
     text-align: left;
+    transform: translateY(0.5rem);
 
     &:hover {
       color: $tertiary;
     }
 
     &.nuxt-link-active {
+      padding: 1rem;
       background: $dark;
       color: $light;
+      transform: translateY(0);
       box-shadow: inset 0px -5px 5px -4px rgba(0,0,0,0);
+      cursor: default;
     }
   }
 }
