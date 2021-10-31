5fd5a6bac9660eae8e53145d0de127ab99901dc7
MrHatch <christof@hierwirdgeloungt.de>
2021-10-31 21:30:21 +0100

    added rudimentary error page

---
+++ layouts/error.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="page">
+    <div class="container text-center">
+      <h1 v-if="error.statusCode === 404">
+        Oh no! Page not found.
+      </h1>
+      <h1 v-else>
+        Oh no! An error occurred.
+      </h1>
+      <NuxtLink to="/">
+        Back to home page
+      </NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'error',
+  props: ['error'], // you can set a custom layout for the error page
+};
+</script>
